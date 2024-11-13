cf97c48ddfc64cd8eb029fe62d9421bc4a23f83b
GenricAblo <ablo.genric2020@gmail.com>
2024-11-13 18:47:09 +0400

    This code simply displays the values of three values inside a single print() function

--- Assessment 1/AbloGenric-Assessment-1/01-CodingisCool.py
+++ Assessment 1/AbloGenric-Assessment-1/01-CodingisCool.py
@@ -1,5 +1,5 @@
 #adding value to these three variables 
-word1, word2, word3 = "Coding", "is", "Cool"
+word1, word2, word3 = "Coding", "is", "Cool" 
 
 # Use string concatenation to combine the variables and print the phrase
 print(word1, word2, word3 )
